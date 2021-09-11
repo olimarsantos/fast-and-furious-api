160c5dd4cb9b60305833bb0e1f8814fcea173cb2
Olimar dos Santos <CIANDT\olimar@lnb029021cps.ciandt.global>
2021-09-10 21:04:48 -0300

    feat: base project

---
+++ src/main/kotlin/com/olimar/fastandfuriousapi/FastAndFuriousApiApplication.kt
@@ -0,0 +1,11 @@
+package com.olimar.fastandfuriousapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastAndFuriousApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<FastAndFuriousApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/olimar/fastandfuriousapi/FastAndFuriousApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.olimar.fastandfuriousapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastAndFuriousApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
